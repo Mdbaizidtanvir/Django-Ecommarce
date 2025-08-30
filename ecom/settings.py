c7632b975cbd783be50710f4295138a1d882d2d8
Mdbaizidtanvir <mdbaizidtanvir@gmail.com>
2025-08-30 19:49:01 +0600

    Your commit message here.

--- ecom/settings.py
+++ ecom/settings.py
@@ -125,12 +125,22 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
+
+import dj_database_url
+from decouple import config
+import os
+
+DATABASE_URL = config("DATABASE_URL", default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}")
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        DATABASE_URL,
+        conn_max_age=600,
+        ssl_require=DATABASE_URL.startswith("postgres://") or DATABASE_URL.startswith("postgresql://")
+    )
 }
+
 
 
 # Password validation
